38a1df0b9756158bed180b10dd81ded63d60dbad
aemeltsev <h002mes@gmail.com>
2020-05-07 19:12:05 +0300

    update

--- I2CDMA/inc/I2C1Module.h
+++ I2CDMA/inc/I2C1Module.h
@@ -1,15 +1,36 @@
 /**
   ******************************************************************************
   * @file    I2C1Module.h
-  * @author  IRQ
-  * @version V0.1
-  * @date    05-March-2019
-  * @brief   Provides a library to access a HD44780-based character LCD module.
+  * @author  Anthony Emeltsev
+  * @version V0.2
+  * @date    May-2020
+  * @brief   STM32F10X, I2C, DMA, Master Mode.
+	*
+	* The MIT License (MIT)
+  *
+  * Copyright (c) 2020 Anthony Emeltsev
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
   ******************************************************************************
   */
 	
 #include "stm32f10x.h"
-#include <stdint.h>
 
 /* Define data and control lines  */
 #define LAST    ((uint8_t)0x01)
--- I2CDMA/src/I2C1Module.c
+++ I2CDMA/src/I2C1Module.c
@@ -1,10 +1,32 @@
 /**
   ******************************************************************************
   * @file    I2C1Module.c
-  * @author  IRQ
-  * @version V0.1
-  * @date    March-2019
-  * @brief   Master Mode, 7-bit Address.
+  * @author  Anthony Emeltsev
+  * @version V0.2
+  * @date    May-2020
+  * @brief   STM32F10X, I2C, DMA, Master Mode.
+	*
+	* The MIT License (MIT)
+  *
+  * Copyright (c) 2020 Anthony Emeltsev
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
   ******************************************************************************
   */
 
@@ -474,4 +496,3 @@ void I2C_DMA_TX(uint16_t regaddr, uint8_t *data_ptr, uint16_t data_length, uint8
 	
 	I2C1->CR2 &= ~I2C_CR2_ITERREN;                 /*Close itterupt I2C=>DMA*/
 }
-	
